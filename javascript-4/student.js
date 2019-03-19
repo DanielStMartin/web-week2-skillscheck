8123b5cda99f2ce2d277fea0a57b675d7d9409f7
Daniel St. Martin <dan-iel@Daniels-MacBook-Air.local>
2019-03-19 15:14:08 -0700

    Skillz that killz

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -5,32 +5,51 @@
 // Create a new array called 'faveColors' and set it your three favorite colors as strings.
 
 // CODE HERE
-
+var faveColors = ["red, blue, black"];
 //////////////////Step 2////////////////////
-// Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, profileImage. 
-
-// The firstName key should be your name as a string. 
-
-// The superHeroName key should be something fun, feel free to jazz it up e.g. "Bodacious Bryan". 
-
-// The homeTown key should be your home town. 
-
+// Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, profileImage.
+var me = {
+  firstName: null,
+  superHeroName: null,
+  homeTown: null,
+  superPowers: null,
+  superPowerXP: null,
+  profileImag: null
+};
+// The firstName key should be your name as a string.
+me.firstName = "Daniel";
+// The superHeroName key should be something fun, feel free to jazz it up e.g. "Bodacious Bryan".
+me.superHeroName = "Munchlax";
+// The homeTown key should be your home town.
+me.homeTown = "Nonya";
 // The superPowers key should be an array of your three super powers as a string. e.g. ['skateboarding', 'crime fighting', 'crime inciting']
-
-//  The superPowerXP key will be a method that returns a random number between 1-100. Use Math.floor(Math.random() * 100) + 1 
+me.superPowers = ["sleeping", "narping", "munching"];
+//  The superPowerXP key will be a method that returns a random number between 1-100. Use Math.floor(Math.random() * 100) + 1
+me.superPowerXP = function() {
+  Math.floor(Math.random() * 100) + 1;
+};
 
 // The profileImage key will return this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
 // CODE HERE
-
+me.profileImage = function() {
+  return `https://randomuser.me/api/portraits/med/lego/${Math.floor(
+    Math.random() * 10
+  ) + 1}.jpg`;
+};
 //////////////////Step 3////////////////////
 // Create three variables to hold some data off your me object. The first variable should be 'regularName' that is the value of your firstName on the me object. The next variable is 'superName' which is the value of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
 
 // CODE HERE
-
+var regularName = me.firstName;
+var superName = me.superHeroName;
+var homeTown = me.homeTown;
 //////////////////Step 4////////////////////
-//Create a function called 'setColor' that takes in one param called arr. Just to make sure that we only ever have three colors, use splice to trim the array to 3 colors. 
-
+//Create a function called 'setColor' that takes in one param called arr. Just to make sure that we only ever have three colors, use splice to trim the array to 3 colors.
+function setColor(arr) {
+  array.length - 3;
+}
+array.length - 1;
 // Next, create a for loop to loop over the array. If any of the colors you chose is 'blue', change it's value to '#4D4DFF' which is just a more appealing color of blue. Outside of the for loop but still inside of setColor, invoke the function called background which will take in three arguments. These arguments should be each item of your array. This is a function we created for you to set the background colors.
 
 // CODE HERE
@@ -39,7 +58,6 @@
 //Create a function called 'setPowers' that takes in arr as a parameter. Loop over the arr param and run a function called createLi(), which will take each item of the array as an agument. The createLi function is a function we created to set the data on the screen. It outside the scope of this project
 
 // CODE HERE
-
 
 //////////////////Step 6////////////////////
 //Lastly, create a function called redactInfo that will take in an obj as a paramter. Let's imagine our super hero needs to go undercover and we need to remove all info about them. That is what this function will do. Loop over the object and change each keys value to 'redacted'. Outside of the loop but still in the redactInfo function, run the function redacted() which is a function we created that will update the text on the screen.
